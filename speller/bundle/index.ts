87545b2eea08421444cfd6d1d4528745799f552b
Dylan Hand <dylan.hand@gmail.com>
2024-02-28 21:25:35 +1000

    Test if the path is making it angry

--- speller/bundle/index.ts
+++ speller/bundle/index.ts
@@ -114,7 +114,8 @@ async function run() {
         core.debug(builder.build())
 
         const payloadPath = await makeInstaller("./install.iss")
-        core.setOutput("payload-path", payloadPath)
+        // core.setOutput("payload-path", payloadPath)
+        core.setOutput("payload-path", "whyyoumad")
         core.debug(`Installer created at ${payloadPath}`)
     } else if (spellerType == SpellerType.MacOS) {
         const payloadPath = await DivvunBundler.bundleMacOS(spellername, version, packageId, langTag, spellerPaths)
