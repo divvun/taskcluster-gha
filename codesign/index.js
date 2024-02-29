a196870440e453f0d464d59b9d4227792149cc8f
Dylan Hand <dylan.hand@gmail.com>
2024-02-29 15:28:57 +1000

    Try removing quotes

--- codesign/index.js
+++ codesign/index.js
@@ -43,7 +43,7 @@ async function run() {
     if (process.platform == "win32") {
         core.debug("  Windows platform");
         const signedFilePath = `${filePath}.signed`;
-        exec.exec("curl", ["-v", "-X", "POST", "-F", `"file=@${filePath}"`, "http://192.168.122.1:5000", "-o", `${signedFilePath}`]);
+        exec.exec("curl", ["-v", "-X", "POST", "-F", `file=@${filePath}`, "http://192.168.122.1:5000", "-o", `${signedFilePath}`]);
         exec.exec("dir");
         core.setOutput("signed-path", signedFilePath);
     }
