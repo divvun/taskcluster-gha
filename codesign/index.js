6e1af9736edff123ae193d6168365e055f19b562
Dylan Hand <dylan.hand@gmail.com>
2024-02-29 15:38:49 +1000

    Windows really doesn't like listing files

--- codesign/index.js
+++ codesign/index.js
@@ -44,7 +44,6 @@ async function run() {
         core.debug("  Windows platform");
         const signedFilePath = `${filePath}.signed`;
         exec.exec("curl", ["-v", "-X", "POST", "-F", `file=@${filePath}`, "http://192.168.122.1:5000", "-o", `${signedFilePath}`]);
-        exec.exec("dir");
         core.setOutput("signed-path", signedFilePath);
     }
     else if (process.platform === "darwin") {
