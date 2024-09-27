da3abed05c245656f9b36495530fd42c3c093757
Flammie A Pirinen <flammie@iki.fi>
2024-09-27 13:45:44 +0200

    can I has simple debug print?

--- lang/check/index.ts
+++ lang/check/index.ts
@@ -10,9 +10,17 @@ async function run() {
     }
     const directory = path.join(githubWorkspace, "lang")
     await Bash.runScript("make check -j$(nproc)", { cwd: path.join(directory, "build") })
+    Bash.runScript("cat tools/spellcheckers/test/fstbased/desktop/hfst/test-suite.log", { cwd: path.join(directory, "build") })
 }
 
 run().catch(err => {
     console.error(err.stack)
+    const githubWorkspace = process.env.GITHUB_WORKSPACE
+    if (githubWorkspace == null) {
+        core.setFailed("GITHUB_WORKSPACE not set, failing.")
+        return
+    }
+    const directory = path.join(githubWorkspace, "lang")
+    Bash.runScript("cat tools/spellcheckers/test/fstbased/desktop/hfst/test-suite.log", { cwd: path.join(directory, "build") })
     process.exit(1)
 })
