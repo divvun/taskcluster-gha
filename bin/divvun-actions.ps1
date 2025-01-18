1c80cd70b874060fa135c7de243f43796d6a8e52
Brendan Molloy <brendan@necessary.nu>
2025-01-18 16:12:49 +0100

    Handle resetting dir because pwsh is dumb

--- bin/divvun-actions.ps1
+++ bin/divvun-actions.ps1
@@ -1,14 +1,19 @@
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
-$CWD = Get-Location
+$CWD = [Environment]::CurrentDirectory
 $SCRIPT_DIR = Split-Path -Parent $PSScriptRoot
 
-Set-Location -Path $SCRIPT_DIR
+try {
+    Set-Location -Path $SCRIPT_DIR
 
-$platform = if ($env:_DIVVUN_ACTIONS_PLATFORM) { $env:_DIVVUN_ACTIONS_PLATFORM } else { "windows" }
-$environment = if ($env:_DIVVUN_ACTIONS_ENV) { $env:_DIVVUN_ACTIONS_ENV } else { "native" }
+    $platform = if ($env:_DIVVUN_ACTIONS_PLATFORM) { $env:_DIVVUN_ACTIONS_PLATFORM } else { "windows" }
+    $environment = if ($env:_DIVVUN_ACTIONS_ENV) { $env:_DIVVUN_ACTIONS_ENV } else { "native" }
 
-$env:_DIVVUN_ACTIONS_PWD = $CWD
-$scriptArgs = if ($args.Count -eq 0) { @('-h') } else { $args }
-& deno run -A --unstable-sloppy-imports index.ts $scriptArgs
+    $env:_DIVVUN_ACTIONS_PWD = $CWD
+    $scriptArgs = if ($args.Count -eq 0) { @('-h') } else { $args }
+    & deno run -A --unstable-sloppy-imports index.ts $scriptArgs
+} finally {
+    [Environment]::CurrentDirectory = $CWD
+}
+
